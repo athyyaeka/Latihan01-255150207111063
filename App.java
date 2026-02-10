0b917bf029b140cd118639fd0c772a906bf71e33
Athiya <athiyaez2007@gmail.com>
2026-02-10 12:55:13 +0700

    Update dimensions for BujurSangkar and Lingkaran

--- App.java
+++ App.java
@@ -1,12 +1,13 @@
 package LATIHAN;
 public class App {
     public static void main(String[] args) {
-        BujurSangkar bs = new BujurSangkar("Bujur Sangkar", 4);
+        BujurSangkar bs = new BujurSangkar("Bujur Sangkar", 6);
         bs.info();
         System.out.println("Luas: " + bs.luas());
 
-        Lingkaran ling = new Lingkaran("Lingkaran", 7);
+        Lingkaran ling = new Lingkaran("Lingkaran", 9);
         ling.info();
         System.out.println("Luas: " + ling.luas());
     }
 }
+
